e9237834e25238dd73d32bef7b25f7642b125096
windyday <63159472+CoverUp137@users.noreply.github.com>
2024-01-31 11:44:19 +0800

    Update diy2.sh

--- 6.0/x86/diy2.sh
+++ 6.0/x86/diy2.sh
@@ -11,8 +11,20 @@
 #
 
 # Modify default IP
-sed -i ' s/192.168.1.1/192.168.2.9/g ' package/base-files/files/bin/config_generate
-sed -i ' s/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g ' target/linux/x86/Makefile
-# 修改内核版本
-# sed -i ' s/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g ' target/linux/x86/Makefile
-#sed -i ' s/luci-theme-bootstrap/luci-theme-argone/g ' feeds/luci/collections/luci/Makefile
+sed -i 's/192.168.1.1/192.168.2.9/g' package/base-files/files/bin/config_generate
+
+#修改内核版本
+sed -i ' s/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g ' target/linux/x86/Makefile
+# 删除源
+# sed -i '/kenzok/d' /etc/opkg/distfeeds.conf
+# sed -i '/small/d' /etc/opkg/distfeeds.conf
+#sed -i 's/luci-theme-bootstrap/luci-theme-neobird/g' feeds/luci/collections/luci/Makefile
+
+sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
+ 
+# 修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='windyday_op'' package/lean/default-settings/files/zzz-default-settings
+ 
+# 加入编译者信息
+sed -i "s/OpenWrt /Windyday build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+ 
