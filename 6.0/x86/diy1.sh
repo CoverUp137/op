a63285eea95a651b080e0b39c5f6ab64fbf55f3f
windyday <63159472+CoverUp137@users.noreply.github.com>
2024-11-10 11:16:21 +0800

    Update diy1.sh

--- 6.0/x86/diy1.sh
+++ 6.0/x86/diy1.sh
@@ -22,6 +22,7 @@
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
- sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+
