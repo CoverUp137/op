793243bece9cafb219932e4355084b4554bb0ee9
windyday <63159472+CoverUp137@users.noreply.github.com>
2024-11-12 01:19:00 +0800

    Update diy1.sh

--- 6.0/x86/diy1.sh
+++ 6.0/x86/diy1.sh
@@ -25,5 +25,11 @@
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git opentopd https://github.com/CoverUp137/sirpdboy-package.git' feeds.conf.default
+echo 'src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+git clone https://github.com/CoverUp137/sirpdboy-package.git package/sirpdboy-package
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+#git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
+git clone https://github.com/sirpdboy/myautocore package/myautocore
