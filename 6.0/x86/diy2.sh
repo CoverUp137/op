29689e9fb66c0f236e9295db2730e16c4fd47994
windyday <63159472+CoverUp137@users.noreply.github.com>
2024-11-28 01:09:01 +0800

    Update diy2.sh

--- 6.0/x86/diy2.sh
+++ 6.0/x86/diy2.sh
@@ -15,13 +15,13 @@
 sed -i 's/192.168.1.1/192.168.2.3/g' package/base-files/files/bin/config_generate
 
 #修改内核版本
-#sed -i ' s/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g ' target/linux/x86/Makefile
+sed -i ' s/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g ' target/linux/x86/Makefile
 # 删除源
 # sed -i '/kenzok/d' /etc/opkg/distfeeds.conf
 # sed -i '/small/d' /etc/opkg/distfeeds.conf
 #sed -i 's/luci-theme-bootstrap/luci-theme-neobird/g' feeds/luci/collections/luci/Makefile
 
-#sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
  
 # 修改默认主机名
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='windyday_op'' package/lean/default-settings/files/zzz-default-settings
