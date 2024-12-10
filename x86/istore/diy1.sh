ef1e204385ff5790427e6a92f8fdac743b8639e0
windyday <63159472+CoverUp137@users.noreply.github.com>
2024-12-10 08:55:32 +0800

    Update diy1.sh

--- x86/istore/diy1.sh
+++ x86/istore/diy1.sh
@@ -26,14 +26,14 @@
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
 #echo 'src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' >> feeds.conf.default
-#echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-git clone https://github.com/CoverUp137/sirpdboy-package.git package/sirpdboy-package
-git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+#git clone https://github.com/CoverUp137/sirpdboy-package.git package/sirpdboy-package
+#git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 #git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
-git clone https://github.com/sirpdboy/myautocore package/myautocore
+#git clone https://github.com/sirpdboy/myautocore package/myautocore
 #git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
