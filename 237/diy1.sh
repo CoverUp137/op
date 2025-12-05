978e2f058aeec2986b89f5f6475f5c6b0f7a5cfa
windyday <63159472+CoverUp137@users.noreply.github.com>
2025-12-05 12:22:22 +0800

    Update diy1.sh

--- 237/diy1.sh
+++ 237/diy1.sh
@@ -5,6 +5,5 @@
 # 一键分区
 # git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 git clone https://github.com/linkease/istore package/istore
