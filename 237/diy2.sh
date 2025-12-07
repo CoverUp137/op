7a286ff4cc774f1420ea02b760d71937a2ef79f5
windyday <63159472+CoverUp137@users.noreply.github.com>
2025-12-08 06:00:21 +0800

    Update diy2.sh

--- 237/diy2.sh
+++ 237/diy2.sh
@@ -1,1 +1,2 @@
 sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i "/set system\.@system\[-1\]\.hostname='/s/ImmortalWrt/WindyDay/" package/base-files/files/bin/config_generate
