9636b67042c4f1bc1dc77888ba5016b7aa95b7cc
windyday <63159472+CoverUp137@users.noreply.github.com>
2025-12-09 21:28:43 +0800

    Update diy1.sh

--- 237/diy1.sh
+++ 237/diy1.sh
@@ -1,6 +1,6 @@
 # echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 # 上网时间控制
-git clone https://github.com/sirpdboy/luci-app-timecontrol package/luci-app-timecontrol
+# git clone https://github.com/sirpdboy/luci-app-timecontrol package/luci-app-timecontrol
 
 # 一键分区
 # git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
@@ -12,7 +12,7 @@ git clone https://github.com/linkease/istore package/istore
 git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
 
 # luci-app-wizard
-git clone https://github.com/sirpdboy/luci-app-netwizard.git package/luci-app-netwizard
+# git clone https://github.com/sirpdboy/luci-app-netwizard.git package/luci-app-netwizard
 
 # 看门狗
 git clone https://github.com/sirpdboy/luci-app-watchdog package/watchdog
