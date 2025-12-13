2f597ce1cf6556a30795f066204c98d7d4cab9fb
windyday <63159472+CoverUp137@users.noreply.github.com>
2025-12-14 00:45:03 +0800

    Update diy1.sh

--- 237/diy1.sh
+++ 237/diy1.sh
@@ -20,12 +20,5 @@
 # echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 # echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.defaul
 #store商店
-#git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/linkease/istore.git package/istore
 
-
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default 
-./scripts/feeds update istore
-./scripts/feeds install -d y -p istore luci-app-store
-
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
