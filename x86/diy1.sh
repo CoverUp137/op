f5cd357fcee154a4ec7a8c62c2dc0d6a599e5841
windyday <63159472+CoverUp137@users.noreply.github.com>
2025-12-17 05:15:20 +0800

    Update diy1.sh

--- x86/diy1.sh
+++ x86/diy1.sh
@@ -7,3 +7,4 @@ echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >>
 
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
