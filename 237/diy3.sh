2e8a25454aed7fcb266a4aeb940fca99cd226682
windyday <63159472+CoverUp137@users.noreply.github.com>
2025-12-21 01:50:11 +0800

    Update diy3.sh

--- 237/diy3.sh
+++ 237/diy3.sh
@@ -5,6 +5,6 @@ echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 #echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
 
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+#git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/linkease/istore.git package/istore
-git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
+#git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
