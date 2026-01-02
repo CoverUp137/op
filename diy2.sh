0944dd4624305a8f030b2a8a03096ef35aaa4c9f
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-01-03 06:13:04 +0800

    Change default IP from 192.168.6.1 to 192.168.124.1

--- diy2.sh
+++ diy2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.6.1/192.168.2.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.124.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/LEDE/N60-PRO/g' package/base-files/files/bin/config_generate 
 
