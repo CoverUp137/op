2fc22e16608c44b82161c986eaf28af13da3a1f5
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-01-07 09:08:30 +0800

    Fix sed command for config_generate in diy2.sh

--- 237/diy2.sh
+++ 237/diy2.sh
@@ -1,3 +1,2 @@
 sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/N60-Pro/g' package/base-files/files/bin/config_generate 
-sed -i '8aPKG_FORTIFY_SOURCE=0' package/feeds/packages/libxcrypt/Makefile
