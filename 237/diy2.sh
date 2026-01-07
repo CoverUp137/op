42ba193903cf114cec1f1a0b8de51d1b1cb6a187
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-01-07 22:29:35 +0800

    Update diy2.sh

--- 237/diy2.sh
+++ 237/diy2.sh
@@ -1,7 +1,2 @@
 sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/N60-Pro/g' package/base-files/files/bin/config_generate 
-
-sed -i "/_('CPU usage (%)')/a\
-    _('固件编译作者'), 'Windy Day',\
-    _('项目地址'), '<a href=\"https://github.com/CoverUp137/op\" target=\"_blank\">GitHub</a>',\
-" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
