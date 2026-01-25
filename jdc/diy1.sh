499189b8ceff8c0445bd35656672a5859478ba79
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-01-25 14:36:29 +0800

    Update diy1.sh

--- jdc/diy1.sh
+++ jdc/diy1.sh
@@ -3,3 +3,6 @@
 # git clone https://github.com/linkease/istore package/istore
 
 
+
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
