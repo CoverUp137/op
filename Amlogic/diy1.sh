eb40b830f0ef72b2d2edd888b17e3cac97e46d85
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-02-17 06:25:41 +0800

    Update diy1.sh

--- Amlogic/diy1.sh
+++ Amlogic/diy1.sh
@@ -3,4 +3,3 @@ git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/linkease/istore.git package/istore
 git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
 
-git clone https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
