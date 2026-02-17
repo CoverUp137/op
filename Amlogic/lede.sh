55497e0bfd9d381ce21557abe17d4fc34f3cc6da
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-02-17 19:33:53 +0800

    Update lede.sh

--- Amlogic/lede.sh
+++ Amlogic/lede.sh
@@ -1,5 +1,3 @@
-rm -rf feeds/luci/applications/luci-app-netdata
-git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/linkease/istore.git package/istore
