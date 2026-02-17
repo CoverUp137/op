ec4da2a79a96366e9a1800a134c89fd7834d4d82
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-02-17 11:59:20 +0800

    Add files via upload

---
+++ Amlogic/lede.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/linkease/istore.git package/istore
