01d4922b102fffef21ad669c0a5157f8265cb71f
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-02-18 18:23:41 +0800

    Update lede.sh

--- Amlogic/lede.sh
+++ Amlogic/lede.sh
@@ -2,3 +2,4 @@ rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
