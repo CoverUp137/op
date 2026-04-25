e7d6d7a16f272e4d4306ca64a20997c875aab56b
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-04-26 02:24:52 +0800

    Add cloning of luci-app-daed repository

--- jdc/diy1.sh
+++ jdc/diy1.sh
@@ -9,3 +9,4 @@ git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
 git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 # git clone https://github.com/sirpdboy/luci-app-watchdog package/watchdog
 git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
