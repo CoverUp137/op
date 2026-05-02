5754980b572e49811664b0ac59bc9af7b1353d42
windyday <63159472+CoverUp137@users.noreply.github.com>
2026-05-02 13:44:26 +0800

    Update diy1.sh

--- 237/diy1.sh
+++ 237/diy1.sh
@@ -17,8 +17,10 @@ git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-part
 
 # git clone https://github.com/papagaye744/luci-theme-design package/luci-theme-design
 
-# echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-# echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.defaul
+
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+
 #store商店
 git clone https://github.com/linkease/istore.git package/istore
 
